d20d81662fabf7842cd0f9dd8954e371c06801e5
N SAI GANESH <116795720+NALLAKUKKALASAIGANESH@users.noreply.github.com>
2024-04-29 17:38:18 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Agecalculator from './components/Agecalculator';
+
+function App() {
+  return (
+    <div className="App">
+      <Agecalculator/>
+    </div>
+  );
+}
+
+export default App;
